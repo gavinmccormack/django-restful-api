7efb8a41537af9ae68d52e090775fb163cc56066
TheGrimJam <gavin.mccormack@gmail.com>
2022-05-04 17:59:02 +0100

    Updates the secret key to be less secret

--- eventr/eventr/settings.py
+++ eventr/eventr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)xoonm366a4g^h!9!6+rdii$=k!ghh3mrwnwt!fvc&=2kkl!p4"
+SECRET_KEY = "weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeenotsosecret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
