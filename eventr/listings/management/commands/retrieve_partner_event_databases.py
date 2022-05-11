cb9cbc7da9ebe7a5b7e70721e2a5139233590179
TheGrimJam <gavin.mccormack@gmail.com>
2022-05-11 21:02:22 +0100

    removes comments

--- eventr/listings/management/commands/retrieve_partner_event_databases.py
+++ eventr/listings/management/commands/retrieve_partner_event_databases.py
@@ -29,37 +29,24 @@ def retrieve_partner_events(feed):
 
 def upload_events(events, source):
 	""" Takes JSON object events, and database object source """
-	# Get DB object for this source
 	source_name = source.spektrix_client_name
 	for event in events:
 		external_api_id = event['id']
 		name = event['name']
-		duration = event['duration'] # What is running time?
-		description = event['description'] # Description/html description - can't know which is relevant
+		duration = event['duration']
+		description = event['description']
 		event_obj = Event.objects.get_or_create(source=source, foreign_id=external_api_id, name=name, duration_minutes=duration, description=description)
 		
-		# Instance times here like 9 - 10 may. I'm not clear on first/last instance time, the interval of shows, etc. 
-		# For this I am just going to assume "firstInstanceDateTime" per entry is enough to handle eventInstances
-		# Would confirm if this wasn't a demo project, but doesn't seem too important a detail.
-		# Same goes for venue name and address. I'm not sure if it's implied that these would be part of the "source"
-		# Or if the mapping of API val: DB value would vary based on the source.
-		# Going to skip past
 		start_datetime = event['firstInstanceDateTime'] venue_name = "Venue name"
 		venue_address = "Address"
 		event_instance = EventInstance.objects.get_or_create(foreign_id=external_api_id, event=event_obj[0], start_datetime=start_datetime, venue_name=venue_name, venue_address=venue_address)
-		# Also for this, I thought the API would include some kind of unique ID for each event, but duplicate events for separate instances don't seem to share
-		# anything that would make a good primary key for event objects
-		# From here, just moving onto to DRF views instead of nitpicking the model:api relationship
-
+		
 class Command(BaseCommand):
 	help = "Retrieves data from partner APIs and inserts them into the database"
 
 	def handle(self, *args, **options):
 		PARTNER_EVENT_APIS = Source.objects.all()
-    	#print(PARTNER_EVENT_APIS[0].api_url)
 		for feed in PARTNER_EVENT_APIS:
 			events = retrieve_partner_events(feed)
 			upload_events(events, feed)
 
-
-# For toppingbooks there is a unique ID field. 
